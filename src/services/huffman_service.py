80150ecf881fcb12aa1b193c4d653a0cc04922c3
VoxBorealis <uusitalo.y@gmail.com>
2022-09-15 13:24:12 +0300

    Add early steps of Huffman algo

--- src/services/huffman_service.py
+++ src/services/huffman_service.py
@@ -1,3 +1,4 @@
+from queue import PriorityQueue
 
 class HuffmanService:
     """Class responsible for the logic of the Huffman compression
@@ -14,11 +15,11 @@ class HuffmanService:
         Args:
             file (_.txt_): _A .txt file that will be compressed_
         """
-        sorted = self._frequency_sort()
-        print(sorted)
+        sorted = self._calculates_frequency()
+        tree = self._build_tree(sorted)
 
-    def _frequency_sort(self):
-        """Sorts all the characters from a text file in a dictionary based on their frequency
+    def _calculates_frequency(self):
+        """Calculates the frequency of each character and returns an array of Tuples (count (int), character (string))
 
         Args:
             file (_.txt_): __
@@ -30,8 +31,53 @@ class HuffmanService:
         with open(self.file) as f:
             contents = f.read()
             print(contents)
+            for char in contents:
+                if char not in char_dict:
+                    char_dict[char] = 1
+                else:
+                    char_dict[char] = char_dict[char] + 1
+        char_freq_list = []
 
-        return char_dict
+        for c in char_dict.items():
+            char_freq_list.append((c[1], c[0]))
+
+        return char_freq_list
+
+    def _build_tree(self, char_freq):
+        tree = PriorityQueue()
+        for c in char_freq:
+            tree.put(Node(c))
+
+        print("--------------")
+        while not tree.empty():
+            print(tree.get().value)
+
+        return tree
+
+class Node:
+    
+    def __init__(self, value, left_child = None, right_child = None):
+        self.value = value
+        self.left_child = left_child
+        self.right_child = right_child
+    
+    def __eq__(self, other):
+        return (self.value[0] == other.value[0])
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __lt__(self, other):
+        return (self.value[0] < other.value[0])
+
+    def __gt__(self, other):
+        return (self.value[0] > other.value[0])
+
+    def __le__(self, other):
+        return (self.value[0] < other.value[0]) or (self.value[0] == other.value[0])
+
+    def __ge__(self, other):
+        return (self.value[0] > other.value[0]) or (self.value[0] == other.value[0])
 
     
 huffman_service = HuffmanService()
